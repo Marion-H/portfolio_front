f7d47918c5eb496d2a1d7b8b83474a249f4a8653
Marion-H <doudou6500@gmail.com>
2020-07-22 21:50:56 +0200

    add component Apropos

--- src/components/MyRouter.jsx
+++ src/components/MyRouter.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Col, Row } from "reactstrap";
+
 import Sidebar from "./sidebar/Sidebar";
+import Apropos from "./Apropos";
 
 import styles from "./sidebar/sidebar.module.css";
-import { Col, Row } from "reactstrap";
 
 export default function MyRouter() {
   return (
@@ -13,6 +15,10 @@ export default function MyRouter() {
           <Col sm="2">
             <Sidebar />
           </Col>
+
+          <Switch>
+            <Route path="/apropos" component={Apropos} />
+          </Switch>
         </Row>
       </Router>
     </div>
