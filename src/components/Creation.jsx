ab98105c59212b504e6fed932887a51f9b271b18
Marion-H <doudou6500@gmail.com>
2020-07-23 11:26:24 +0200

    update componenent projects and add react html parser module

--- src/components/Creation.jsx
+++ src/components/Creation.jsx
@@ -1,6 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Spinner } from "reactstrap";
 import Axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+
+const descriptions = [
+  {
+    text: ``,
+  },
+  {
+    text: `<p>Projet de groupe Front-end et Back-end</p>
+    <p>
+      - Création d'une application pour la gestion de données entre
+      patients et médecins
+    </p>
+    <p>
+      - Création d'une API pour stocker les données (NodeJs,
+      Express, Sequelize, mysql)
+    </p>
+    <p>
+      - Front-end : React , Reacstrap - Envoie, récupération et
+      modification de données via une API (Axios) 
+    </p>
+    <p>- Gestion de projet : Git, Github</p>`,
+  },
+];
 
 export default function Creation() {
   const [projects, setProjects] = useState([]);
@@ -31,7 +54,10 @@ export default function Creation() {
       {projects.map((project) => (
         <Row>
           <Col>
-            {project.description}
+            {project.title}
+            <Row>
+              <Col>{ReactHtmlParser(project.description)}</Col>
+            </Row>
             <Row>
               <Col>
                 <a
@@ -45,7 +71,9 @@ export default function Creation() {
             </Row>
           </Col>
           <Col>
-            <img src={project.image} alt={project.title} />
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <img src={project.image} alt={project.title} width="100%" />
+            </a>
           </Col>
         </Row>
       ))}
